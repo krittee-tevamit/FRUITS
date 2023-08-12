b6586b9f8ed970ecdf3e07f41184d46f78f53128
krittee-tevamit <ktevamit@gmail.com>
2023-08-12 16:03:13 +0700

    first commit

---
+++ FRUITS/Extensions/Color.swift
@@ -0,0 +1,8 @@
+//
+//  Color.swift
+//  FRUITS
+//
+//  Created by codinglife365 on 12/8/2566 BE.
+//
+
+import Foundation
---
+++ FRUITS/View/FruitCardView.swift
@@ -0,0 +1,20 @@
+//
+//  FruitCardView.swift
+//  FRUITS
+//
+//  Created by codinglife365 on 12/8/2566 BE.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FruitCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitCardView()
+    }
+}
