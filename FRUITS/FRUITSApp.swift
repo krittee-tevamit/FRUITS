5d2446abdde119c919d8611208985ae84e9a4002
krittee-tevamit <ktevamit@gmail.com>
2023-08-12 12:25:27 +0700

    Initial Commit

---
+++ FRUITS/FRUITSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FRUITSApp.swift
+//  FRUITS
+//
+//  Created by codinglife365 on 12/8/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct FRUITSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
