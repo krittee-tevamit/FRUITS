c3d1b7342d5ea9e8a4478ed433697bbc50c1ac6b
krittee-tevamit <ktevamit@gmail.com>
2023-08-13 15:44:08 +0700

    Create FruitDetailView

---
+++ FRUITS/View/SourceLinkView.swift
@@ -0,0 +1,30 @@
+//
+//  SourceLinkView.swift
+//  FRUITS
+//
+//  Created by codinglife365 on 13/8/2566 BE.
+//
+
+import SwiftUI
+
+struct SourceLinkView: View {
+    var body: some View {
+        GroupBox() {
+            HStack {
+                Text("Content source")
+                Spacer()
+                Link("Wikipedia", destination: URL(string: "https://wikipedia.com")!)
+                Image(systemName: "arrow.up.right.square")
+            }
+            .font(.footnote)
+        }
+    }
+}
+
+struct SourceLinkView_Previews: PreviewProvider {
+    static var previews: some View {
+        SourceLinkView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
